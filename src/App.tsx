cdf5543381d637e18385422b6f54eedb99f6d68c
Mount-Blanc <mountblanc87@gmail.com>
2022-06-22 16:34:50 -0700

    created barebones structure of pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route} from "react-router-dom"
 import { Container } from "react-bootstrap"
-
-
+import {Home} from './pages/Home'
+import {Store} from './pages/Store'
+import {About} from './pages/About'
 
 function App() {
-  return <Container>
+  return <Container className='mb-4'>
   <Routes>
       <Route path='/' element ={<Home />} />
       <Route path='/store' element ={<Store />} />
