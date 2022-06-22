ba97aad7862f3061d770ac3077e30c3996ea746f
Mount-Blanc <mountblanc87@gmail.com>
2022-06-22 16:42:56 -0700

    created Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import {Container, Navbar as NavbarBs} from "react-bootstrap"
+
+export function NavBar () {
+    return 
+    <NavbarBs className="bg-white shadow-sm mb-3">
+        <Container>Nav</Container>
+     </NavbarBs>
+}
