ae10f77cea0474dece21cc6da4eb4b5c7ed5ca9f
Mount-Blanc <mountblanc87@gmail.com>
2022-06-23 10:20:00 -0700

    finished store item tsx

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,1 +1,17 @@
-export function StoreItem({ id, name, price, imgUrl}) {}
+import { Card } from "react-bootstrap"
+
+type StoreItemProps = {
+    id:number
+    name:string
+    price:number
+    imgUrl:string
+}
+
+
+export function StoreItem({ id, name, price, imgUrl}:StoreItemProps) {
+return <Card>
+    <Card.img variant='top' src={imgUrl} height='200px' style={
+        {objectFit:'cover'}
+    }
+</Card>
+}
